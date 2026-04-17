3113bf283873ce5e41d3a9199cd021b65c14188f
cathyccc <cathyky.chan@gmail.com>
2026-04-17 16:07:44 -0400

    created Login class, created login tests

---
+++ pages/LoginPage.ts
@@ -0,0 +1,27 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export class LoginPage {
+  private readonly page: Page;
+  private readonly emailInput: Locator;
+  private readonly passwordInput: Locator;
+  private readonly loginButton: Locator;
+  private readonly errorMessage: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.emailInput = page.getByPlaceholder('Username');
+    this.passwordInput = page.getByPlaceholder("Password");
+    this.loginButton = page.getByRole('button', { name: "login" });
+    this.errorMessage = page.locator('[data-test="error"]');
+  }
+
+  async goto() {
+    await this.page.goto('/')
+  }
+
+  async login(email: string, pass: string) {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(pass);
+    await this.loginButton.click();
+  }
+}
---
+++ tests/login.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+test('user with correct credentials can log in successfully @smoke @e2e', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'secret_sauce');
+  await expect(page).toHaveURL(/.*inventory/);
+  const title = page.locator('.title');
+  await expect(title).toHaveText('Products');
+})
+
+test('an error message should show for user with incorrect credentials @regression', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'incorrectPW');
+  await expect(loginPage.errorMessage).toBeVisible();
+})
