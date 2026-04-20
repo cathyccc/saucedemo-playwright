68b1c0ed407570c4b34a00dced777067d559b8a3
cathyccc <cathyky.chan@gmail.com>
2026-04-20 10:29:44 -0400

    refactored text matching to use built-in matchers

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -14,16 +14,14 @@ test('an error message should show for user with incorrect credentials @regressi
   const loginPage = new LoginPage(page);
   await loginPage.goto();
   await loginPage.login('standard_user', 'incorrectPW');
-  const message = await loginPage.getErrorMessage();
   await expect(loginPage.errorMessage).toBeVisible();
-  await expect(message).toContain("Epic sadface: Username and password do not match any user in this service")
+  await expect(loginPage.errorMessage).toContainText("Username and password do not match any user in this service")
 })
 
 test('an error message should show for a locked out user @regression', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
   await loginPage.login('locked_out_user', 'secret_sauce');
-  const message = await loginPage.getErrorMessage();
   await expect(loginPage.errorMessage).toBeVisible();
-  await expect(message).toContain("Epic sadface: Sorry, this user has been locked out.")
+  await expect(loginPage.errorMessage).toContainText("user has been locked out")
 })
